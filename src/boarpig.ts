d8cf5509bda77a499bc5e4a41265a6c4a897a673
sieve <68281563+s-i-e-v-e@users.noreply.github.com>
2020-09-08 23:34:51 +0000

    Add fmt command

--- src/boarpig.ts
+++ src/boarpig.ts
@@ -80,7 +80,7 @@ async function extract_pdf(file: string, out_dir?: string) {
 
     const cwd = Deno.cwd();
     try {
-        console.log(`Extracting ${file} into ${out_dir}`);
+        println(`Extracting ${file} into ${out_dir}`);
         Deno.chdir(out_dir);
         const p = Deno.run({
           cmd: ["mutool", "extract", file_full_path],
@@ -100,7 +100,7 @@ async function extract_djvu(file: string) {
 
   make_out_dir(out_dir);
 
-  console.log(`Converting ${file} => ${out_file}`);
+  println(`Converting ${file} => ${out_file}`);
   const p = Deno.run({
     cmd: ["ddjvu", "-format=pdf", file, out_file],
   });
@@ -127,7 +127,7 @@ function renumber(file: string, prefix: string, range: string) {
   let i = 1;
   xs.forEach(x => {
     const y = `${prefix}${pad(i)}${EXT}`;
-    console.log(`Renaming ${x} => ${y}`);
+    println(`Renaming ${x} => ${y}`);
 
     const xx = `${out_dir}/${x}`;
     const yy = `${out_dir}/${y}`;
@@ -137,16 +137,48 @@ function renumber(file: string, prefix: string, range: string) {
 }
 
 async function process_text_file(file: string) {
-	let a = await Deno.readTextFile(file);
+  let a = undefined;
+  try {
+    a = await Deno.readTextFile(file);
+  }
+  catch(e) {
+
+  }
+  if (a === undefined) return;
+  println(`Format ${file}`);
 	a = a.trim();
-	a = a.replaceAll(/([a-zA-Z])’([a-zA-Z])/gi, "$1'$2");
-	Deno.writeTextFile(file, a);
+  a = a.replaceAll(/([a-zA-Z])’([a-zA-Z])/gi, "$1'$2");
+  a = a.replaceAll(' ?', '?');
+  a = a.replaceAll(' !', '!');
+  a = a.replaceAll(' ;', ';');
+  a = a.replaceAll(' :', ':');
+
+  a = a.replaceAll('“ ', '“');
+  a = a.replaceAll(' ”', '”');
+  a = a.replaceAll('‘ ', '‘');
+  a = a.replaceAll(' ’', '’');
+
+  a = a.replaceAll('““', '“');
+  a = a.replaceAll('””', '”');
+
+  a = a.replaceAll('‘‘', '“');
+  a = a.replaceAll('’’', '”');
+  
+  a = a.replaceAll('‘“', '“');
+  a = a.replaceAll('”’', '”');
+
+  a = a.replaceAll('“‘', '“');
+  a = a.replaceAll('’”', '”');
+
+  a = a.replaceAll(/[ ]+/g, ' ');
+  a = a.replaceAll(/ \n/g, '\n');
+  a = a.replaceAll(/\n\n+/g, '\n\n');
+  Deno.writeTextFile(file, a);
 }
 
 interface Page { image: string, text: string }
 
 function ocr(file: string, prefix?: string, range?: string) {
-  const EXT = '.png';
   Deno.env.set("OMP_THREAD_LIMIT", "1");
   const dir = dirPart(file);
   const name = namePart(file);
@@ -157,7 +189,7 @@ function ocr(file: string, prefix?: string, range?: string) {
       const p = Deno.run({
         cmd: ["tesseract", "--dpi", "300", "-l", "eng", `${out_dir}/${x.image}`, `${out_dir}/${x.text}`],
       });
-      console.log(`${x.image} => ${x.text}`);
+      println(`OCR ${x.image} => ${x.text}`);
       return p.status();
     });
     
@@ -170,9 +202,7 @@ function ocr(file: string, prefix?: string, range?: string) {
 
     const n1 = 0|xs.length/4;
     const n2 = xs.length%4;
-    console.log(n1);
-    console.log(n2);
-  
+    
     const zs: Page[][] = [];
     
     for (let i = 0; i < n1; i++) {
@@ -197,8 +227,15 @@ function ocr(file: string, prefix?: string, range?: string) {
   }
 
   const xs = build_batch(listRenumberedImageFiles(out_dir, prefix, range));
-  xs.forEach(x => console.log(x));
 	xs.forEach(x => run(x));
+}
+
+function fmt(file: string, prefix?: string, range?: string) {
+  const dir = dirPart(file);
+  const name = namePart(file);
+  const out_dir = `${dir}/${name}_output`;
+  const xs = listRenumberedImageFiles(out_dir, prefix, range)
+  xs.forEach(async x => process_text_file( `${out_dir}/${namePart(x)}.txt`));
 }
 
 function serve(file: string, port?: string) {
@@ -211,22 +248,27 @@ function serve(file: string, port?: string) {
   serve_http(base_dir, out_dir, p);
 }
 
+function println(x: string) {
+  console.log(x);
+}
+
 function version() {
-  console.log('boarpig 0.1');
-  console.log('Copyright (C) 2020 Sieve (https://github.com/s-i-e-v-e)');
-  console.log('This is free software; see the source for copying conditions.  There is NO');
-  console.log('warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.');
+  println('boarpig 0.1');
+  println('Copyright (C) 2020 Sieve (https://github.com/s-i-e-v-e)');
+  println('This is free software; see the source for copying conditions.  There is NO');
+  println('warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.');
 }
 
 function help() {
   version();
-  console.log('Usage:');
-  console.log('help,    --help,                   Display this information.');
-  console.log('version, --version                 Display version information.');
-  console.log('extract file                       Extract images from pdf/djvu file.');
-  console.log('renumber file prefix start-end     Renumber extracted images within given range.');
-  console.log('ocr file [prefix [start-end]]      Renumber extracted images within given range.');
-  console.log('serve file [port]                    Serve http over port.');
+  println('Usage:');
+  println('help,    --help,                   Display this information.');
+  println('version, --version                 Display version information.');
+  println('extract file                       Extract images from pdf/djvu file.');
+  println('renumber file prefix start-end     Renumber extracted images within given range.');
+  println('ocr file [prefix [start-end]]      Perform OCR on renumbered images.');
+  println('fmt file [prefix [start-end]]      Format generated text files.');
+  println('serve file [port]                  Serve http over port.');
 }
 
 function main(args: string[]) {
@@ -235,6 +277,7 @@ function main(args: string[]) {
         case "extract": extract(args[1]); break;
         case "renumber": renumber(args[1], args[2], args[3]); break;
         case "ocr": ocr(args[1], args[2], args[3]); break;
+        case "fmt": fmt(args[1], args[2], args[3]); break;
         case "serve": serve(args[1], args[2]); break;
         case "--version":
         case "version": version(); break;
